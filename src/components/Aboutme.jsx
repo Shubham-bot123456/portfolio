be6d3c7743f8512019027946f8d3e565916c4096
Shubham Jadhav <sjadhav11999@gmail.com>
2023-11-13 10:17:27 +0530

    version 0.1

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -3,26 +3,8 @@ import { AiFillCloseCircle } from "react-icons/ai";
 import objective from "./objectiveimg.png";
 
 export default function Aboutme() {
-  const [originalState, setOriginalState] = useState(false);
-  return originalState ? (
-    <div className="mx-14 my-10 py-10 px-10 flex justify-evenly shadow-xl bg-[#A9DFBF] rounded-lg hover:bg-gray-500 hover:text-white hover:transition-colors">
-      {/* photo */}
-      <div>
-        <img src={objective} className="w-[400px] h-[200px]"></img>
-      </div>
-      <div className="text-left my-auto mx-5x">
-        <h2 className="font-sans font-bold text-xl">Objective</h2>
-        <p className="my-2 mx-3">
-          Motivated and skilled Java Backend Developer with 2 years of hands-on
-          experience in building high-performance and scalable server-side
-          applications. Proficient in Java, Spring Framework, and SQL databases.
-          Seeking a challenging role to contribute my expertise in software
-          development and problem-solving to deliver innovative solutions.
-        </p>
-      </div>
-    </div>
-  ) : (
-    <div className="mx-14 my-10 py-10 px-10 flex justify-evenly shadow-xl bg-red-500 rounded-lg hover:bg-gray-500 hover:text-white hover:transition-colors">
+  return (
+    <div className="mx-14 my-10 py-10 px-10 flex justify-evenly shadow-xl bg-red-500 rounded-lg hover:bg-gray-500 hover:text-white hover:transition-colors border-white border-4 border-double border-spacing-3">
       {/* photo */}
       <div className="text-left">
         <div className="flex justify-between">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,11 +9,24 @@ import workimg from "./work3.png";
 import toolsused from "./toolsused.png";
 import skills from "./skills.png";
 export default function Header() {
-  const [boolean, setboolean] = useState(false);
+  const [boolean, setboolean] = useState(true);
   const [hamburgerboolean, sethamburgerboolen] = useState(false);
   const [mode, setMode] = useState("objective");
+  const [aboutMeButtonStyle, setStyleAboutMe] = useState(
+    "text-white bg-gray-800 rounded-xl px-10 py-2.3 font-semibold  hover:text-teal-600 hover:transition-color"
+  );
+  useEffect(() => {
+    if (boolean)
+      setStyleAboutMe(
+        "text-white bg-gray-800 rounded-xl px-10 py-2.3 font-semibold  hover:text-teal-600 hover:transition-color border-2 border-teal-600 "
+      );
+    else
+      setStyleAboutMe(
+        "text-white bg-gray-800 rounded-xl px-10 py-2.3 font-semibold  hover:text-teal-600 hover:transition-color "
+      );
+  }, [boolean]);
   return (
-    <div className="bg-gray-950 w-screen">
+    <div className="bg-gray-950 h-screen">
       <div className="flex justify-between px-5 py-3 ">
         <div className="flex justify-evenly">
           <PiHamburgerDuotone
@@ -39,7 +52,7 @@ export default function Header() {
           </ul>
 
           <button
-            className="text-white bg-gray-800 rounded-xl px-10 py-2.3 font-semibold  hover:text-teal-600 hover:transition-color"
+            className={aboutMeButtonStyle}
             onClick={() => setboolean(!boolean)}
           >
             {boolean ? "about me (ON)" : "about me (OFF)"}
@@ -50,35 +63,47 @@ export default function Header() {
         {hamburgerboolean ? (
           <div className="h-screen bg-[#C70039]  text-white py-2 text-left transition-transform ease-in duration-1000 opacity-100">
             <ul>
-              <div className="flex justify-between hover:text-red-400 hover:transition-color mx-2  rounded-lg border-white border-spacing-11 hover:bg-gray-800">
-                <li
-                  className="py-3 ml-8  mr-12  font-semibold hover:text-red-400 hover:transition-color"
-                  onClick={() => setMode("objective")}
-                >
+              <div
+                onClick={() => {
+                  setMode("objective");
+                  setboolean(false);
+                }}
+                className="flex justify-between hover:text-red-400 hover:transition-color mx-2  rounded-lg border-white border-spacing-11 hover:bg-gray-800"
+              >
+                <li className="py-3 ml-8  mr-12  font-semibold hover:text-red-400 hover:transition-color">
                   Objective
                 </li>
               </div>
-              <div className="flex justify-between hover:text-red-400 hover:transition-color mx-2  rounded-lg border-white border-spacing-11 hover:bg-gray-800">
-                <li
-                  className="py-3 ml-8 mr-12 font-semibold hover:text-red-400 hover:transition-color "
-                  onClick={() => setMode("workexperience")}
-                >
+              <div
+                onClick={() => {
+                  setMode("workexperience");
+                  setboolean(false);
+                }}
+                className="flex justify-between hover:text-red-400 hover:transition-color mx-2  rounded-lg border-white border-spacing-11 hover:bg-gray-800"
+              >
+                <li className="py-3 ml-8 mr-12 font-semibold hover:text-red-400 hover:transition-color ">
                   Work experience
                 </li>
               </div>
-              <div className="flex justify-between hover:text-red-400 hover:transition-color mx-2  rounded-lg border-white border-spacing-11 hover:bg-gray-800">
-                <li
-                  className="py-3 ml-8 mr-12 font-semibold hover:text-red-400 hover:transition-color"
-                  onClick={() => setMode("toolsused")}
-                >
+              <div
+                onClick={() => {
+                  setMode("toolsused");
+                  setboolean(false);
+                }}
+                className="flex justify-between hover:text-red-400 hover:transition-color mx-2  rounded-lg border-white border-spacing-11 hover:bg-gray-800"
+              >
+                <li className="py-3 ml-8 mr-12 font-semibold hover:text-red-400 hover:transition-color">
                   Tools Used
                 </li>
               </div>
-              <div className="flex justify-between hover:text-red-400 hover:transition-color mx-2  rounded-lg border-white border-spacing-11 hover:bg-gray-800">
-                <li
-                  className="py-3 ml-8 mr-12 font-semibold hover:text-red-400 hover:transition-color"
-                  onClick={() => setMode("projectsundertaken")}
-                >
+              <div
+                onClick={() => {
+                  setMode("projectsundertaken");
+                  setboolean(false);
+                }}
+                className="flex justify-between hover:text-red-400 hover:transition-color mx-2  rounded-lg border-white border-spacing-11 hover:bg-gray-800"
+              >
+                <li className="py-3 ml-8 mr-12 font-semibold hover:text-red-400 hover:transition-color">
                   Skills
                 </li>
               </div>
@@ -93,20 +118,27 @@ export default function Header() {
           ) : (
             <>
               {mode === "objective" ? (
-                <div className="mx-14 my-10 py-10 px-10 flex justify-evenly shadow-xl bg-[#A9DFBF] rounded-lg hover:bg-gray-500 hover:text-white hover:transition-colors">
+                <div className="mx-14 my-auto py-10 px-10  shadow-xl  rounded-lg hover:transition-colors">
                   {/* photo */}
                   <div>
-                    <img src={objective} className="w-[400px] h-[200px]"></img>
+                    <img
+                      src={objective}
+                      className="w-[150px] h-[100px] mx-auto"
+                    ></img>
                   </div>
-                  <div className="text-left my-auto mx-5x">
-                    <h2 className="font-sans font-bold text-xl">Objective</h2>
-                    <p className="my-2 mx-3">
+                  <div className="text-middle  my-auto mx-5x">
+                    <h2 className="font-sans text-teal-600 font-bold text-xl">
+                      Objective
+                    </h2>
+                    <p className="my-2 mx-3 text-slate-500">
                       Motivated and skilled Java Backend Developer with 2 years
                       of hands-on experience in building high-performance and
-                      scalable server-side applications. Proficient in Java,
-                      Spring Framework, and SQL databases. Seeking a challenging
-                      role to contribute my expertise in software development
-                      and problem-solving to deliver innovative solutions.
+                      scalable server-side applications.
+                      <br />
+                      Proficient in Java, Spring Framework, and SQL databases.
+                      Seeking a challenging role to contribute my expertise in
+                      software development and problem-solving to deliver
+                      innovative solutions.
                     </p>
                   </div>
                 </div>
@@ -120,30 +152,33 @@ export default function Header() {
                     </h2>
                     <p className="my-2 mx-3">
                       Java Backend Developer, Tata Consultancy Services , Pune ,
-                      Maharashtra. (Oct 2021 – Present)  Developed and
-                      maintained RESTful APIs for a large-scale logistic and
-                      mail platform using Spring Boot, resulting in improved
-                      user experience and increased customer satisfaction. 
-                      Implemented caching mechanisms both in memory and
-                      centralized (Redis) and optimized database queries,
-                      leading to a 20% reduction in API response times. 
+                      Maharashtra. (Oct 2021 – Present) <br />
+                      Developed and maintained RESTful APIs for a large-scale
+                      logistic and mail platform using Spring Boot, resulting in
+                      improved user experience and increased customer
+                      satisfaction. <br /> Implemented caching mechanisms both
+                      in memory and centralized (Redis) and optimized database
+                      queries, leading to a 20% reduction in API response times.
+                      <br />
                       Collaborated with the frontend team to design and
                       implement API contracts, ensuring seamless integration
-                      between frontend and backend components.  Demonstrated
-                      strong problem-solving skills in identifying and resolving
-                      complex issues within the application. Reduced production
-                      defects by 30% .  Conducted unit testing and participated
-                      in code reviews to maintain code quality and identify
-                      potential issues.  Utilized SQL queries and Hibernate ORM
-                      to manage and optimize the database operations.  Managed
-                      project dependencies efficiently using Maven/Gradle,
-                      ensuring a smooth build process and easy collaboration
-                      with team members.  Collaborated effectively in an Agile
-                      development environment using Scrum, participating in
-                      sprint planning and contributing to sprint goals. 
-                      Leveraged frameworks and libraries like Hibernate, Lombok,
-                      and Spring Cloud to enhance application functionality and
-                      development efficiency.  Maintained clear and
+                      between frontend and backend components. <br />
+                      Demonstrated strong problem-solving skills in identifying
+                      and resolving complex issues within the application.
+                      Reduced production defects by 30% . <br /> Conducted unit
+                      testing and participated in code reviews to maintain code
+                      quality and identify potential issues. <br />
+                      Utilized SQL queries and Hibernate ORM to manage and
+                      optimize the database operations. <br /> Managed project
+                      dependencies efficiently using Maven/Gradle, ensuring a
+                      smooth build process and easy collaboration with team
+                      members. <br />
+                      Collaborated effectively in an Agile development
+                      environment using Scrum, participating in sprint planning
+                      and contributing to sprint goals. <br /> Leveraged
+                      frameworks and libraries like Hibernate, Lombok, and
+                      Spring Cloud to enhance application functionality and
+                      development efficiency. <br /> Maintained clear and
                       comprehensive technical documentation for the codebase and
                       APIs using Atlassian Confluence, facilitating better
                       knowledge sharing within the team.
@@ -160,11 +195,12 @@ export default function Header() {
                   <div className="text-left my-auto mx-5x">
                     <h2 className="font-sans font-bold text-xl">Tools Used</h2>
                     <p className="my-2 mx-3">
-                       IDE : Intellij Idea, Eclipse , Visual Studio Code ,
+                      IDE : Intellij Idea, Eclipse , Visual Studio Code ,
                       Sublime Text , VIM.  Database Clients : DBeaver(Universal
-                      Client), Pg Admin.  API Testing : Postman.  Git GUI
-                      Tools : Git Tortoise, Git GUI  Other Tools : Notepad++,
-                      Beyond compare,
+                      Client), Pg Admin. <br />
+                      API Testing : Postman. <br /> Git GUI Tools : Git
+                      Tortoise, Git GUI <br />
+                      Other Tools : Notepad++, Beyond compare,
                     </p>
                   </div>
                 </div>
@@ -175,18 +211,23 @@ export default function Header() {
                   <div className="text-left my-auto mx-5x">
                     <h2 className="font-sans font-bold text-xl">Skills</h2>
                     <p className="my-2 mx-3">
-                       Programming Languages: Java, Javascript, SQL.  Java
-                      Frameworks: Spring Boot, Hibernate, React JS (Basics) 
-                      Well versed with IDE such as Intellij idea , Eclipse. 
-                      RESTful API Design and Implementation  Database
-                      Management: PostgreSQL  Version Control: Git  Build
-                      Tools: Maven  Heavy user of Terminal based editors such
-                      as VIM (NeoVim).  Familiarity with Agile Methodologies 
-                      Problem-Solving and Troubleshooting Skills  Strong Team
-                      Player and Effective Communicator  Valuable experience
-                      working with the AWS Services such as EC2, S3, RDS,
-                      Lambda, SQS, SNS, VPC, EFS.  Among top 10% of Top typist
-                      in the world with reference to TypeRacer.
+                      Programming Languages: Java, Javascript, SQL. <br /> Java
+                      Frameworks: Spring Boot, Hibernate, React JS (Basics){" "}
+                      <br />
+                      Well versed with IDE such as Intellij idea , Eclipse.{" "}
+                      <br />
+                      RESTful API Design and Implementation <br /> Database
+                      Management: PostgreSQL <br />
+                      Version Control: Git <br />
+                      Build Tools: Maven <br /> Heavy user of Terminal based
+                      editors such as VIM (NeoVim). <br /> Familiarity with
+                      Agile Methodologies <br />
+                      Problem-Solving and Troubleshooting Skills <br /> Strong
+                      Team Player and Effective Communicator <br />
+                      Valuable experience working with the AWS Services such as
+                      EC2, S3, RDS, Lambda, SQS, SNS, VPC, EFS. <br />
+                      Among top 10% of Top typist in the world with reference to
+                      TypeRacer.
                     </p>
                   </div>
                 </div>
